2f68fee1db1dcb87d91e991bf82e5cc7a1972f8d
IhebKesraoui <iheb.kesraoui@nanoz-group.com>
2024-05-22 10:17:47 +0200

    dvc repro

--- src/tdms_to_csv.py
+++ src/tdms_to_csv.py
@@ -2,16 +2,16 @@ from pathlib import Path
 
 from nptdms import TdmsFile
 
-input_folder = Path('./dvc cml pipeline data preprossses/data')
+input_folder = Path('./data')
 
-output_folder = './dvc cml pipeline data preprossses/out'
+output_folder = './out'
 
 tdms_rglob = input_folder.rglob('*.tdms')
 list_rglob = list(tdms_rglob)
 list_rglob.sort(key=lambda i: int(i.name.split('_')[-1].split('.')[0]))
-
+print (input_folder)
 for tdms_file in list_rglob:
-    
+    print('01')
     # Read data as dataframe
     tdms_df = TdmsFile.read(tdms_file).as_dataframe()
 

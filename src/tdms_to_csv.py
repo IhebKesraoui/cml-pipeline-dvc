111715c55e0e0f584323cfe5c19f4395fc644f6d
IhebKesraoui <iheb.kesraoui@nanoz-group.com>
2024-05-22 17:06:44 +0200

    dd

--- src/tdms_to_csv.py
+++ src/tdms_to_csv.py
@@ -11,12 +11,9 @@ list_rglob = list(tdms_rglob)
 list_rglob.sort(key=lambda i: int(i.name.split('_')[-1].split('.')[0]))
 print (input_folder)
 for tdms_file in list_rglob:
-    print(' 01')
-    # Read data as dataframe
     tdms_df = TdmsFile.read(tdms_file).as_dataframe()
 
-    # Save extracted data as csv file
     csv_file_path = Path(output_folder, tdms_file.stem+'.csv')
     tdms_df.to_csv(csv_file_path)
 
-print('Finidzsh\n')
+print('Finidzccccsh\n')

6393f46c6f8dff5bc66ecdc31b5655b7e0a63503
IhebKesraoui <iheb.kesraoui@nanoz-group.com>
2024-05-23 09:58:03 +0200

    uplode the code v1

--- src/tdms_to_csv.py
+++ src/tdms_to_csv.py
@@ -2,26 +2,25 @@ from pathlib import Path
 from nptdms import TdmsFile
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaIoBaseUpload
+import io
 import os
 import json
 
+# Get service account key JSON from environment variable
+service_account_info = json.loads(os.environ['GDRIVE_CREDENTIALS_DATA'])
+
 # Authenticate using service account credentials
-service_account_file = '/home/nanoz-admin/Documents/trypipeline/cml-pipeline-dvc/.dvc/default.json'
-credentials = service_account.Credentials.from_service_account_file(
-    service_account_file,
+credentials = service_account.Credentials.from_service_account_info(
+    service_account_info,
     scopes=['https://www.googleapis.com/auth/drive']
 )
 
 # Build the Google Drive service
 drive_service = build('drive', 'v3', credentials=credentials)
 
-# Define input and output folders
+# Define input folder
 input_folder = Path('./data')
-output_folder = Path('./out')
-
-# Create output folder if it doesn't exist
-output_folder.mkdir(parents=True, exist_ok=True)
 
 # Get list of tdms files
 tdms_rglob = input_folder.rglob('*.tdms')
@@ -36,16 +35,17 @@ for tdms_file in list_rglob:
     # Read tdms file
     tdms_df = TdmsFile.read(tdms_file).as_dataframe()
 
-    # Convert to CSV and save to local disk
-    csv_file_path = output_folder / f'{tdms_file.stem}.csv'
-    tdms_df.to_csv(csv_file_path)
+    # Convert to CSV and create in-memory file
+    csv_buffer = io.StringIO()
+    tdms_df.to_csv(csv_buffer, index=False)
+    csv_buffer.seek(0)  # Move to the beginning of the StringIO buffer
 
     # Upload CSV file to Google Drive in the specified folder
     file_metadata = {
         'name': f'{tdms_file.stem}.csv',
         'parents': [destination_folder_id]
     }
-    media = MediaFileUpload(csv_file_path, mimetype='text/csv')
+    media = MediaIoBaseUpload(csv_buffer, mimetype='text/csv')
     drive_service.files().create(
         body=file_metadata,
         media_body=media,
